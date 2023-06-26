fa761ea3c5ce8626f522febc5a2aaed2a4e32d2c
Shovon Basak <shvnbasak@gmail.com>
2023-06-27 01:01:30 +0600

    Removed tkinter

--- app/models/votes.py
+++ app/models/votes.py
@@ -1,4 +1,3 @@
-from tkinter import CASCADE
 from sqlalchemy import Column, ForeignKey, Integer
 from app.database import Base
 
@@ -6,5 +5,5 @@ from app.database import Base
 class Vote(Base):
     __tablename__ = "votes"
 
-    post_id = Column(Integer, ForeignKey('posts.id', ondelete=CASCADE), primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id', ondelete=CASCADE), primary_key=True)
+    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key=True)
