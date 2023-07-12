17451f95a04f3f3ea0e632199b950b33e0a977bf
Shovon Basak <shvnbasak@gmail.com>
2023-07-12 23:17:27 +0600

    Build CI/CD job added for Github action

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     database_name: str
     secret_key: str
     algorithm: str
-    token_expiration: int
+    access_token_expire_minutes: int
 
     class Config:
         env_file = ".env"
